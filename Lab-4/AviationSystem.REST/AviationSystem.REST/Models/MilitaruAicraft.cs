ac0888f4d816dba31c3a09186ba840a2062d02c5
FrekBol <129262560+FrekBol@users.noreply.github.com>
2025-05-30 06:45:36 +0300

    Add files via upload

---
+++ Lab-4/AviationSystem.REST/AviationSystem.REST/Models/MilitaruAicraft.cs
@@ -0,0 +1,13 @@
+﻿namespace AviationSystem.REST.Models
+{
+    // Models/MilitaryAircraftModel.cs
+    public class MilitaryAircraftModel : AircraftModel
+    {
+        public double MaxGForce { get; set; }           // Максимальне допустиме навантаження G
+        public bool HasStealthCapability { get; set; }  // Стелс-технології
+        public int NumberOfHardpoints { get; set; }     // Кількість пілонів для озброєння
+        public IEnumerable<string> StandardArmaments { get; set; }
+        // Перелік штатних озброєнь (ракети, бомби)
+        public string RadarType { get; set; }           // Тип РЛС
+    }
+}
